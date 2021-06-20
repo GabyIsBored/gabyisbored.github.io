975e9db0a3ccabff72b8319cb63d90a4daa9f7c5
GabyIsBored <79170829+GabyIsBored@users.noreply.github.com>
2021-06-20 20:38:24 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -87,4 +87,5 @@ const preObject = document.getElementById('object');
 const dbRefObject = firebase.database().ref().child('object');
 
 dbReObject.on('value', snap => console.log(snap.val()));
+    
 }());
